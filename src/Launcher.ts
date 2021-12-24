a3d44dea096fa1e8c9f20a62797737f3bce53b96
Miro Adamy <miro.adamy@pivotree.com>
2021-12-24 16:21:15 +0100

    Attempt 1 - broken because of security constraints

--- src/Launcher.ts
+++ src/Launcher.ts
@@ -1,6 +1,12 @@
+import { Router } from "./Router";
+
 class Launcher {
+
+    private router: Router = new Router();
+
     public launchApp(){
-        console.log('Launcher app started')
+        console.log('Launcher app started');
+        this.router.handleRequest();
     }
 }
 
